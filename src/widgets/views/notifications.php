015d944fa0e50ab4e34f764df92783802c502083
Mohammad Hadi Rezaie <91452487+MHR41@users.noreply.github.com>
2023-07-26 18:44:11 +0300

    Sliding the slidebar fixed. Just css file needed to be update.

--- src/widgets/views/notifications.php
+++ src/widgets/views/notifications.php
@@ -9,34 +9,42 @@ NotificationAsset::register($this);
 <?php
 if (count($notifications) > 0)
 { ?>
-    <a href="#" class="dropdown-toggle" style="background-color: #212529" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-        <i class="fa fa-bell"><span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger"><?=  count($notifications) ?>
+
+
+    <ul id="notification">
+        <li class="dropdown nav-item">
+            <a href="#" class="dropdown-toggle" style="background-color: #212529" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                <i class="fa fa-bell"><span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger"><?=  count($notifications) ?>
            <span class="visually-hidden">unread messages</span></span></i>
-    </a>
+            </a>
 
-    <ul class="dropdown-menu notify-drop">
-        <div class="notification-heading">
-            <h4 class="menu-title">Notifications</h4>
-        </div>
-        <hr class="dropdown-divider">
-
-        <div class="drop-content">
-            <div class="notifications-wrapper">
-                <div class="card" role="presentation">
-                    <?php foreach ($notifications as $notification) { ?>
-                        <a class="card" role="presentation" href="/notification/default/view?id_notification=<?= $notification->id_notification?>">
-                                <h4 class="item-title"><?php echo $notification -> title ?></h4>
-                                <p class="item-info"><?php if (strlen($notification->text) > 19) { echo substr($notification -> text, 0, 22).'...';}
-                                                            else echo($notification->text) ?></p>
-                        </a>
-                    <?php } ?>
+            <ul class="dropdown-menu notify-drop">
+                <div class="notification-heading">
+                    <h4 class="menu-title">Notifications</h4>
                 </div>
-            </div>
-        </div>
+                <hr class="dropdown-divider">
 
-        <hr class="dropdown-divider">
-        <a class="notification-footer" href="/notification/default/index?"><h4 class="menu-title-footer">View all</h4></a>
+                <div class="drop-content">
+                    <div class="notifications-wrapper">
+                        <div class="card" role="presentation">
+                            <?php foreach ($notifications as $notification) { ?>
+                                <a class="card" role="presentation" href="/notification/default/view?id_notification=<?= $notification->id_notification?>">
+                                    <h4 class="item-title"><?php echo $notification -> title ?></h4>
+                                    <p class="item-info"><?php if (strlen($notification->text) > 19) { echo substr($notification -> text, 0, 22).'...';}
+                                        else echo($notification->text) ?></p>
+                                </a>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+
+                <hr class="dropdown-divider">
+                <a class="notification-footer" href="/notification/default/index?"><h4 class="menu-title-footer">View all</h4></a>
+            </ul>
+
+        </li>
     </ul>
+
 <?php
 
 }
