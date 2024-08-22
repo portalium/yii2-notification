dbac3ccc7fd8c574e5bdf2836113d7c26c057617
sudesuvar <sudesuvar51@gmail.com>
2024-08-22 11:10:47 +0000

    first commit.

--- src/widgets/Notification.php
+++ src/widgets/Notification.php
@@ -14,6 +14,7 @@ class Notification extends Widget
     public $options;
     public $display;
     public $icon;
+    public $placement;
 
     //initialize widget properties
     public function init()
